844527e66e6879f2e30328b15dae6b7913af9e1f
Manfred Geiler <manfred.geiler@irian.at>
2014-04-03 21:25:04 +0200

    fixed ws endpoint for Ankor 0.2

--- todo-servlet/src/main/java/TodoEndpoint.java
+++ todo-servlet/src/main/java/TodoEndpoint.java
@@ -1,12 +1,18 @@
-import at.irian.ankor.ref.Ref;
-import at.irian.ankor.servlet.websocket.AnkorEndpoint;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import at.irian.ankor.application.Application;
+import at.irian.ankor.system.WebSocketServerEndpoint;
+import org.apache.commons.lang.NotImplementedException;
 
 @SuppressWarnings("unused")
-public class TodoEndpoint extends AnkorEndpoint {
+public class TodoEndpoint extends WebSocketServerEndpoint {
+
     @Override
-    protected Object getModelRoot(Ref rootRef) {
+    protected Application createApplication() {
         // TODO
         throw new NotImplementedException();
+    }
+
+    @Override
+    protected String getPath() {
+        return "/websocket/ankor/{clientId}";
     }
 }
