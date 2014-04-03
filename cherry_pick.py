f2c98c67fb3d8d927028e4c30a0c4e9e0db530a4
Florian Klampfer <f.klampfer@gmail.com>
2014-04-03 19:20:25 +0200

    improve cherry_pick script

--- cherry_pick.py
+++ cherry_pick.py
@@ -1,46 +1,85 @@
 #!/usr/bin/python
 
 import sys
-from subprocess import call
+from subprocess import call as real_call
 
-"""
-def call(call_arguments):
-  print 'Executing: ' + ' '.join(call_arguments)
-  return 0
-"""
+def call(call_args):
+  if sys.flags.debug:
+    print 'Simulating: ' + ' '.join(call_args)
+    return 0
+  else:
+    return real_call(call_args)
+
+option_types = {
+  'include_complete_branch': ['-ic', '--include-complete']
+}
+
+def check_option(options, option_type):
+  flags = option_types[option_type]
+
+  for flag in flags:
+    if flag in options:
+      return True
+
+  return False
 
 def print_usage(): 
   print 'Cherry picks a commit and copies it to a list of branches'
-  print 'Example: python ' + sys.argv[0] + ' 268de3c fx 0 7 server 0 8'
+  print 'Example: python ' + sys.argv[0] + ' --include-complete 268de3c fx 0 7 server 0 8'
 
-args = sys.argv[1:]
+def extract_options(args):
+  options = []
+  for arg in args:
+    if arg.startswith('-'):
+      options.append(arg)
+  return options
 
-if len(args[1:]) % 3 != 0:
-  print_usage()
-  print 'Wrong number of arguments!'
-  exit(1)
+def main():
+  args = sys.argv[1:]
 
-target_commit = args[0]
-ret_code = call(['git', 'checkout', target_commit])
+  if len(args) == 0:
+    print_usage()
+    exit(0)
 
-if ret_code != 0:
-  print "Can't checkout target commit '" + target_commit + "'"
-  exit(1)
+  options = extract_options(args)
 
-for i in range(1, len(args), 3):
-  branch_base = args[i]
-  from_branch = int(args[i + 1])
-  to_branch = int(args[i + 2])
+  args_without_options = args[len(options):]
+  target_commit = args_without_options[0]
+  branch_args = args_without_options[1:]
 
-  for curr_branch_num in range(from_branch, to_branch):
-    curr_branch = branch_base + '-step-' + str(curr_branch_num)
+  if len(branch_args) % 3 != 0:
+    print_usage()
+    print 'Wrong number of arguments!'
+    exit(1)
 
-    ret_code = call(['git', 'checkout', curr_branch])
-    if ret_code != 0:
-      print "Can't checkout target commit '" + curr_branch + "'"
-      exit(0)
+  ret_code = call(['git', 'checkout', target_commit])
+  if ret_code != 0:
+    print "Can't checkout target commit '" + target_commit + "'"
+    exit(1)
 
-    ret_code = call(['git', 'cherry-pick', target_commit])
-    if ret_code != 0:
-      print "Can't cherry-pick '" + target_commit + "' on top of '" + curr_branch + "'"
-      exit(0)
+  for i in range(0, len(branch_args), 3):
+    branch_base = branch_args[i]
+    from_branch = int(branch_args[i + 1])
+    to_branch = int(branch_args[i + 2])
+
+    for curr_branch_num in range(from_branch, to_branch):
+      curr_branch = branch_base + '-step-' + str(curr_branch_num)
+      cherry_pick(curr_branch, target_commit)
+
+    if check_option(options, 'include_complete_branch'):
+      curr_branch = branch_base + '-complete'
+      cherry_pick(curr_branch, target_commit)
+
+def cherry_pick(curr_branch, target_commit):
+  ret_code = call(['git', 'checkout', curr_branch])
+  if ret_code != 0:
+    print "Can't checkout target commit '" + curr_branch + "'"
+    exit(0)
+
+  ret_code = call(['git', 'cherry-pick', target_commit])
+  if ret_code != 0:
+    print "Can't cherry-pick '" + target_commit + "' on top of '" + curr_branch + "'"
+    exit(0)
+
+if __name__ == "__main__":
+    main()
