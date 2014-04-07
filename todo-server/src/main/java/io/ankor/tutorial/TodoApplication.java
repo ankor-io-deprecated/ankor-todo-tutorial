decd8185c40f69882f81ed65885ee82b3a420da2
Florian Klampfer <f.klampfer@gmail.com>
2014-04-07 19:23:51 +0200

    add todo application stub

--- todo-server/src/main/java/io/ankor/tutorial/TodoApplication.java
+++ todo-server/src/main/java/io/ankor/tutorial/TodoApplication.java
@@ -1,9 +1,6 @@
 package io.ankor.tutorial;
 
-import at.irian.ankor.ref.Ref;
 import at.irian.ankor.ref.RefContext;
-import io.ankor.tutorial.model.TaskRepository;
-import io.ankor.tutorial.viewmodel.ModelRoot;
 
 import java.util.Map;
 
@@ -11,7 +8,33 @@ import java.util.Map;
  * @author Manfred Geiler
  */
 public class TodoApplication implements at.irian.ankor.application.Application {
+    @Override
+    public String getName() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
 
-    // TODO
+    @Override
+    public boolean supportsModel(String modelName) {
+        return false;  //To change body of implemented methods use File | Settings | File Templates.
+    }
 
+    @Override
+    public Object lookupModel(String modelName, Map<String, Object> connectParameters) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Object createModel(String modelName, Map<String, Object> connectParameters, RefContext refContext) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void releaseModel(String modelName, Object modelRoot) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void shutdown() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
 }
