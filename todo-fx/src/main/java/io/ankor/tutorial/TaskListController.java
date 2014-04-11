e7f83260073498ffa0b8aee48dea729ede8a7766
Florian Klampfer <f.klampfer@gmail.com>
2014-04-11 14:21:23 +0200

    use refFactory in myInit

--- todo-fx/src/main/java/io/ankor/tutorial/TaskListController.java
+++ todo-fx/src/main/java/io/ankor/tutorial/TaskListController.java
@@ -39,7 +39,7 @@ public class TaskListController implements Initializable {
 
     @ChangeListener(pattern = "root")
     public void myInit() {
-        FxRef rootRef = App.refFactory().ref("root");
+        FxRef rootRef = refFactory().ref("root");
         FxRef modelRef = rootRef.appendPath("model");
         FxRef footerVisibilityRef = modelRef.appendPath("footerVisibility");
 
