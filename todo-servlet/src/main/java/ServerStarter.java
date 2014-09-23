87d2f0c4c5f0cc131da2e0196249aaf8285dd0b8
Florian Klampfer <f.klampfer@gmail.com>
2014-09-23 18:29:28 +0200

    use spring boot to start the server

---
+++ todo-servlet/src/main/java/ServerStarter.java
@@ -0,0 +1,20 @@
+import at.irian.ankor.application.Application;
+import at.irian.ankor.application.GenericApplication;
+import at.irian.ankor.system.WebSocketSpringBootServer;
+import io.ankor.tutorial.viewmodel.ModelRoot;
+import org.springframework.boot.SpringApplication;
+
+public class ServerStarter extends WebSocketSpringBootServer {
+
+    @Override
+    protected Application createApplication() {
+        GenericApplication application = new GenericApplication();
+        application.setName("Todo Sample");
+        application.setDefaultModelType(ModelRoot.class);
+        return application;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerStarter.class, args);
+    }
+}
