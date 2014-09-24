e8cd7c90c085456bd77199637ef6f9d482479c48
Florian Klampfer <f.klampfer@gmail.com>
2014-09-24 17:49:25 +0200

    copy webapp over

---
+++ todo-js-client/src/main/webapp/js/build/main.js
@@ -0,0 +1,11 @@
+/**
+ * @jsx React.DOM
+ */
+define([
+  "ankor/AnkorSystem",
+  "ankor/transport/WebSocketTransport",
+  "ankor/utils/BaseUtils",
+  "react"
+], function (AnkorSystem, WebSocketTransport, BaseUtils, React) {
+  // TODO
+});
